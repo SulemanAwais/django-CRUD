b089d465af8369f228b0f5a3d60da23af45103b9
SulemanAwais <sulemanawais2001@gmail.com>
2024-01-10 14:54:07 +0500

    templates

--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'user': 'Suleman'})
+    users = [
+        {'name': 'Suleman', 'age': 22, 'is_male': True},
+        {'name': 'Salma', 'age': 21, 'is_male': False},
+        {'name': 'Idrees', 'age': 33, 'is_male': False},
+    ]
+    return render(request=request, template_name='hello.html', context={'users': users})
