76ee319bb5bc2d20c383a5a95429dc5faf84dc93
SulemanAwais <sulemanawais2001@gmail.com>
2024-01-10 16:13:50 +0500

    user table

--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db.models import Model, CharField, EmailField, TextField
 
 # Create your models here.
+
+
+class User(Model):
+    firstName: str = CharField(max_length=20)
+    lastName: str = CharField(max_length=20)
+    email: str = EmailField()
+    password: str = TextField()
