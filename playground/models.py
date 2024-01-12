1e99c765b8b5f5a47bff89fef8021e51ee1827ee
SulemanAwais <sulemanawais2001@gmail.com>
2024-01-12 18:47:30 +0500

    User model

--- playground/models.py
+++ playground/models.py
@@ -1,19 +1,9 @@
-from datetime import date
-import datetime
-
-from django.db.models import Model, CharField, EmailField, TextField, DateField
-
-# Create your models here.
-
-
-class User(Model):
-    firstName: str = CharField(max_length=20)
-    lastName: str = CharField(max_length=20)
-    email: str = EmailField()
-    password: str = TextField()
+from django.contrib.auth.models import User
+from django.db.models import Model, CharField, TextField, ForeignKey, SET_NULL
 
 
 class Task(Model):
+    User = ForeignKey(User, on_delete=SET_NULL, null=True, blank=True)
     title: str = CharField(max_length=200, null=False, blank=False)
     content: str = TextField()
     # day: date = DateField(default=date.today())
