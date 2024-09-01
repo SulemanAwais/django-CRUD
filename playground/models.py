5829521b8aab4cad0af3a50ef8d03a549bf1dcee
SulemanAwais <sulemanawais2001@gmail.com>
2024-09-01 07:51:28 +0500

    due_date field added

--- playground/models.py
+++ playground/models.py
@@ -1,9 +1,23 @@
-from django.contrib.auth.models import User
-from django.db.models import Model, CharField, TextField, ForeignKey, SET_NULL
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class Task(Model):
-    User = ForeignKey(User, on_delete=SET_NULL, null=True, blank=True)
-    title: str = CharField(max_length=200, null=False, blank=False)
-    content: str = TextField()
-    # day: date = DateField(default=date.today())
+class Task(models.Model):
+    User = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=200, null=False, blank=False)
+    content = models.TextField()
+    due_date = models.DateField(null=True, blank=True)
+
+    class Status(models.TextChoices):
+        NOT_STARTED = 'NS', _('Not Started')
+        IN_PROGRESS = 'IP', _('In Progress')
+        COMPLETED = 'C', _('Completed')
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.NOT_STARTED,
+    )
+
+    def __str__(self):
+        return self.title
